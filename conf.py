1abed797d6fc44fec5a1296b0abf6f6db0b3ee30
taxguide <singhparash06@gmail.com>
2026-03-17 17:25:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,1 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
+project = 'Tax Software Help'
+author = 'Admin'
+
+extensions = ['myst_parser']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+master_doc = 'index'
+
+html_theme = 'alabaster'
+html_title = "TurboTax Help Guide 2026"
